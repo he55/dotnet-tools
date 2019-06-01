08a296956fa79b59e9ad99eff3548fe518a10cbe
He55 <luck.he55@gmail.com>
2019-06-01 08:26:49 +0800

    Update Program.cs

--- src/dotnet-date/Program.cs
+++ src/dotnet-date/Program.cs
@@ -10,6 +10,7 @@ namespace dotnet_date
             var localTime = DateTimeOffset.Now;
 
             Console.WriteLine("Current date and time:");
+            Console.WriteLine("Current date and time2:");
             Console.WriteLine("{0,-8}: {1}", "Local", localTime.ToString());
             Console.WriteLine("{0,-8}: {1}", "Internet", internetTime?.ToString() ?? "null");
         }
